a861ff42a4a5428010c3a69d0a7bfa7034704e73
Siddhantrawat011 <Sidrawat229@gmail.com>
2025-11-09 09:20:28 +0530

    Added New Service-registry microservice

---
+++ service-registry/service-registry/src/test/java/com/e_learning_platform/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_learning_platform.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
